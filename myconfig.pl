72db18d65a6a4874b704446b41d02efb8e55be9f
mastronardif <mastronardif@gmail.com>
2012-02-17 13:54:21 -0500

    add my config stuff, removed id, pw

---
+++ myconfig.pl
@@ -0,0 +1,7 @@
+# This is myconfig.pl
+pw       => 'mypw',
+id       => 'myid',
+MGApiKey =>  'key-0-rxwnDUMMYf76',
+plugs    => [ 'mail RFC', 'http://www.cpan.org/', 'couldmailin.org', 
+              'mailgun.org', 'http://boilerpipe-web.appspot.com/',
+			  'http://www.pair.com/', 'http://www.activestate.com/stackato', ],
